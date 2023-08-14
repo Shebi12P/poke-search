72149c7b3607eefdb6940eb9cfa3dea93e9353a1
Szymon Brodawka <szymbrod@gmail.com>
2023-08-14 14:08:52 +0200

    Deleting shiny sprite image after searching pokemon with one and chenging text to shiny sprite not avaiable

--- scripts/DOM/infoCard/populateInfoCardImgages.js
+++ scripts/DOM/infoCard/populateInfoCardImgages.js
@@ -1,21 +1,31 @@
+import { checkForNumberInRange } from "../../formValidation/checkForNumberInRange.js";
 import { capitalizeText } from "../../utils/capitalizeText.js";
+import { LAST_BASE_FORM_POKEMON_ID } from "../../variables/lastBaseFormPokemonId.js";
 import { createShinySpriteImage } from "./createShinySpriteImage.js";
 
 export const populateinfoCardImages = (pokemonName, pokemonId) => {
     const BASE_ALT_TEXT = "An image of";
-    const LAST_POKEMON_WITHOUT_SHINY_SPRITE_ID = 1009;
-
+    const FIRST_POKEMON_WITHOUT_SHINY_FORM_ID = 1009;
+    pokemonId = parseInt(pokemonId);
+    
     const defaultSprite = document.querySelector(".default-sprite");
     const defaultSpriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`;
-    
-    const shinySpriteUrl =
-        `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${pokemonId}.png`
-        || null;
     defaultSprite.src = `${defaultSpriteUrl}`;
     defaultSprite.alt = `${BASE_ALT_TEXT} ${capitalizeText(pokemonName)}`;
     
-    if(pokemonId === LAST_POKEMON_WITHOUT_SHINY_SPRITE_ID) return;
+    const isIdInRange = checkForNumberInRange(pokemonId, FIRST_POKEMON_WITHOUT_SHINY_FORM_ID, LAST_BASE_FORM_POKEMON_ID);
+    const shinySpriteTextField = document.querySelector(".shiny-sprite-text");
     
+    if(isIdInRange && document.querySelector(".shiny-sprite")) {
+        document.querySelector(".shiny-sprite").remove();
+        shinySpriteTextField.textContent = "Shiny sprite not avaiable";
+    }
+
+    if(isIdInRange) return;
+    
+    const shinySpriteUrl = 
+        `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${pokemonId}.png`;
+        
     if(!document.querySelector(".shiny-sprite")) {
         createShinySpriteImage();
     }
@@ -23,7 +33,5 @@ export const populateinfoCardImages = (pokemonName, pokemonId) => {
     const shinySprite = document.querySelector(".shiny-sprite");
     shinySprite.src =  `${shinySpriteUrl}`;
     shinySprite.alt =  `${BASE_ALT_TEXT} shiny ${capitalizeText(pokemonName)}`;
-
-    const shinySpriteTextField = document.querySelector(".shiny-sprite-text");
     shinySpriteTextField.textContent = "Shiny sprite";
 }
--- scripts/formValidation/checkForNumberInRange.js
+++ scripts/formValidation/checkForNumberInRange.js
@@ -5,5 +5,7 @@ export const checkForNumberInRange = (number, minRange, maxRange) => {
         isInRange = true;
     }
 
+    console.log(isInRange)
+
     return isInRange;
 }
