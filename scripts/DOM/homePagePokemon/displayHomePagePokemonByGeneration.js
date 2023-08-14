8f95ceafbd2124c46051277c525ba046a27bf9c3
Szymon Brodawka <szymbrod@gmail.com>
2023-08-14 12:18:22 +0200

    Fixing setting the last pokemon in generation attribute in function displaying pokemon by generation

--- scripts/DOM/homePagePokemon/displayHomePagePokemonByGeneration.js
+++ scripts/DOM/homePagePokemon/displayHomePagePokemonByGeneration.js
@@ -5,7 +5,6 @@ import { deletePokemonFromNotCurrentGeneration } from "./deletePokemonFromNotCur
 
 export const displayHomePagePokemonByGeneration = async (event) => {
     const clickedObject = event.target;
-    const clickedObjectSibling = clickedObject.nextElementSibling;
     const wasDropdownItemClicked = clickedObject.classList.contains("dropdown-item");
     if(!wasDropdownItemClicked) return;
 
@@ -16,22 +15,25 @@ export const displayHomePagePokemonByGeneration = async (event) => {
     const firstPokemonInGenerationId = parseInt(
         clickedObject.getAttribute("data-first-pokemon-id-in-generation")
     );
-    let currentLastPokemonInGenerationId = parseInt(clickedObjectSibling
-        .getAttribute("data-first-pokemon-id-in-generation")) - 1;
+    
+    let currentLastPokemonInGenerationId = clickedObject.nextElementSibling
+        ? parseInt(clickedObject.nextElementSibling
+        .getAttribute("data-first-pokemon-id-in-generation")) - 1
+        : LAST_BASE_FORM_POKEMON_ID;
 
-    if(currentLastPokemonInGenerationId < 1) {
+    if(clickedObject.hasAttribute("data-all-pokemon")) {
         currentLastPokemonInGenerationId = LAST_BASE_FORM_POKEMON_ID;
     }
-
+    
     const cardList = document.querySelector(".card-list");
     const previousLastPokemonInGenerationId = cardList.getAttribute("data-last-pokemon-id");
     cardList.setAttribute("data-last-pokemon-id", currentLastPokemonInGenerationId);
 
+    if(firstPokemonInGenerationId === firstGeneratedPokemonId) return;
+
     if(previousLastPokemonInGenerationId > currentLastPokemonInGenerationId) {
         deletePokemonFromNotCurrentGeneration(currentLastPokemonInGenerationId)
     }
-
-    if(firstPokemonInGenerationId === firstGeneratedPokemonId) return;
 
     pokemonCardList.innerHTML = "";
     pokemonCardList.setAttribute("data-first-generated-pokemon-id", firstPokemonInGenerationId);
