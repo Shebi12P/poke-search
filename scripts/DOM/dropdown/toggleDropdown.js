4a6cf7bad256da4735fea7c67b51b913ddadab8e
Szymon Brodawka <szymbrod@gmail.com>
2023-08-14 14:58:48 +0200

    Adding tabing to hint list and dropdown

--- scripts/DOM/dropdown/toggleDropdown.js
+++ scripts/DOM/dropdown/toggleDropdown.js
@@ -1,9 +1,10 @@
 export const toggleDropdown = (event) => {
     const dropdownContainer = event.currentTarget;
     const dropdown = dropdownContainer.querySelector(".dropdown");
+    const dropDownItems = dropdown.querySelectorAll(".dropdown-item");
     const dropdowndIcon = dropdownContainer.querySelector(".dropdown-icon");
     const isExpanded = dropdown.getAttribute("aria-expanded");
-    let expand = "false";
+    let expand = true;
 
     const wasDropdownItemClicked = event.target.classList.contains("dropdown-item");
     if(wasDropdownItemClicked) {
@@ -11,9 +12,21 @@ export const toggleDropdown = (event) => {
         dropdownContainer.querySelector("p").textContent = newDropdownContainerText;
     }
 
-    if(isExpanded === "false") expand = "true";
+    if(isExpanded === "true") {
+        expand = false;
+        dropDownItems.forEach(dropDownItem => {
+            dropDownItem.removeAttribute("tabindex");
+        })
+    }
+    
     const rotate = expand;
 
-    dropdown.setAttribute("aria-expanded", expand);
-    dropdowndIcon.setAttribute("data-rotated", rotate);
+    if(isExpanded === "false") {
+        dropDownItems.forEach(dropdownItem => {
+            dropdownItem.setAttribute("tabindex", "0");
+        })
+    }
+
+    dropdown.setAttribute("aria-expanded", expand.toString());
+    dropdowndIcon.setAttribute("data-rotated", rotate.toString());
 }
--- scripts/DOM/pokemonHintList/populatePokemonHintList.js
+++ scripts/DOM/pokemonHintList/populatePokemonHintList.js
@@ -2,7 +2,7 @@ export const populatePokemonHintList = (pokemonNames) => {
     const pokemonHintList = document.querySelector(".search-pokemon-hint-list");
 
     pokemonNames.forEach(pokemonName => {
-        const pokemonHintElement = `<li class="search-pokemon-hint">${pokemonName}</li>`;
+        const pokemonHintElement = `<li class="search-pokemon-hint" tabindex="0">${pokemonName}</li>`;
         pokemonHintList.insertAdjacentHTML("afterbegin", pokemonHintElement);
     })
 }
