ac7c3a6a0a28ef36a7b135c9b455e2f7343cbb3b
Szymon Brodawka <108801275+Szymon-Brodawka@users.noreply.github.com>
2023-08-28 15:43:46 +0200

    Delete fetchLastPokemonId.js

--- scripts/api/fetchLastPokemonId.js
+++
@@ -1,9 +0,0 @@
-import { fetchPokemon } from "./fetchPokemon.js";
-
-export const fetchLastPokemonId = async () => {
-    const ALL_POKEMON_URL = "?limit=100000&offset=0";
-    const allFetchedPokemon = await fetchPokemon(ALL_POKEMON_URL);
-    const lastPokemonId = allFetchedPokemon.count - 1;
-
-    return lastPokemonId;
-}
